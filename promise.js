959a41183b3cb61f972cff1bbe359f8e670a3abb
desistmrym <desisitimaryam@gmail.com>
2022-06-12 20:40:00 +0700

    add promise

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (elm) => {
+  try {
+    const [responseIXX, responseVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    const res = [...responseIXX, ...responseVGC]
+
+    return res.filter(item => item.hasil === elm).length
+  } catch (err) {
+    console.log('Invalid Sum')
+  }
+};
 
 module.exports = {
   promiseOutput,
